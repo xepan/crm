7c92fd706e4590ce77c44e7cb91ccfe6b8314811
Nillam Joshi <nilam.joshi49@gmail.com>
2016-03-04 15:02:47 +0530

    actions are working like in pending completed ..crm->supportticket..

--- lib/Model/SupportTicket.php
+++ lib/Model/SupportTicket.php
@@ -7,13 +7,16 @@ class Model_SupportTicket extends \xepan\hr\Model_Document{
 		'Pending',
 		'Activated',
 		'Assigned',
-		'Completed'
+		'Completed',
+		'Rejected'
 	];
 	public $actions=[
 		'Pending'=>['view','edit','delete','convert','reject','assign','complete'],
 		'Activated'=>['view','edit','delete','open','reject','assign','complete'],
-		'Assigned'=>['view','edit','delete','open','convert','complete'],
-		'Completed'=>['view','edit','delete','open','convert','reject']
+		'Assigned'=>['view','edit','delete','open','complete','reject'],
+		'Completed'=>['view','edit','delete','open','reject','assign'],
+		'Rejected'=>['view','edit','delete','open','complete','assign']
+
 
 	];
 	function init(){
@@ -24,6 +27,7 @@ class Model_SupportTicket extends \xepan\hr\Model_Document{
 
 		$st_j->addField('title');
 		$st_j->addField('description')->type('text');
+		//$this->getElement('status');
 		$this->getElement('status')->defaultValue('Pending');
 		$this->getElement('created_at')->defaultValue($this->app->now);
 		$this->addCondition('type','SupportTicket');
@@ -32,17 +36,17 @@ class Model_SupportTicket extends \xepan\hr\Model_Document{
 	}
 
 		function assign(){
-		$this['status']='Pending';
+		$this['status']='Assigned';
 
 		$this->app->employee
-			->addActivity("Converted Opportunity", $this->id, $this['ticket_id'])
+			->addActivity("Converted Supportticket", $this->id, $this['ticket_id'])
 			->notifyWhoCan('assign,convert,open','Converted');
 
 		$this->saveAndUnload();
 	}
 
 	function reject(){
-		$this['status']='Pending';
+		$this['status']='Rejected';
 		$this->app->employee
 			->addActivity("Rejected Supportticket", $this->id, $this['ticket_id'])
 			->notifyWhoCan('reject,convert,open','Converted');
@@ -50,9 +54,9 @@ class Model_SupportTicket extends \xepan\hr\Model_Document{
 	}
 
 	function complete(){
-		$this['status']='Pending';
+		$this['status']='Completed';
 		$this->app->employee
-			->addActivity("Completed Opportunity", $this->id, $this['ticket_id'])
+			->addActivity("Completed Supportticket", $this->id, $this['ticket_id'])
 			->notifyWhoCan('reject,convert,open','Converted');
 		$this->saveAndUnload();
 	}
