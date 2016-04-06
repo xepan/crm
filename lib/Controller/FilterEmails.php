460d852feecfceedcc6e8d2f205e0b5740339c1d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-06 17:05:47 +0530

    Catch Hook for Email fetched

---
+++ lib/Controller/FilterEmails.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace xepan\crm;
+
+class Controller_FilterEmails extends \AbstractController {
+
+	function emails_fetched($app,$data){
+		// Emails fetched and now you can track if anything 
+		// 		for new ticket, 
+		// 		to reply rejection
+		// 		or to create a new comment for existing ticket
+		
+	}
+}
--- lib/Initiator.php
+++ lib/Initiator.php
@@ -15,6 +15,9 @@ class Initiator extends \Controller_Addon {
         if($this->app->is_admin){
 	        $m = $this->app->top_menu->addMenu('Crm');
 	        $m->addItem('SupportTicket','xepan_crm_supportticket');
+            $cont = $this->add('xepan\crm\Controller_FilterEmails');
+
+            $this->app->addHook('emails_fetched',[$cont,'emails_fetched']);
 	    }
 	              
     }
