49847375d0006d3276221526bff3a4f05bb2b30a
vijay mali <guddavijay@gmail.com>
2016-04-07 19:16:06 +0530

    define model fields or relations

--- lib/Model/SupportTicket.php
+++ lib/Model/SupportTicket.php
@@ -10,6 +10,7 @@ class Model_SupportTicket extends \xepan\hr\Model_Document{
 		'Completed',
 		'Rejected'
 	];
+	// 'draft','submitted','solved','canceled','assigned','junk'
 	public $actions=[
 		'Pending'=>['view','edit','delete','convert','reject','assign','complete'],
 		'Activated'=>['view','edit','delete','open','reject','assign','complete'],
@@ -21,17 +22,36 @@ class Model_SupportTicket extends \xepan\hr\Model_Document{
 	];
 	function init(){
 		parent::init();
-		$st_j=$this->join('supportticket.document_id');
-		$st_j->hasOne('xepan\commerce\Customer','customer_id');
-		$st_j->hasMany('xepan\communication','communication_id');
+		$this->addCondition('type','SupportTicket');
+		
+		$st_j=$this->join('support_ticket.document_id');
 
+		$st_j->hasOne('xepan\base\Contact','contact_id');
+		$st_j->hasOne('xepan\communication\Communication','communication_id');
+		
+		$st_j->addField('name');
+		$st_j->addField('uid');
+		$st_j->addField('from_id');
+		$st_j->addField('from_email');
+		$st_j->addField('from_name');
 
-		$st_j->addField('title');
-		$st_j->addField('description')->type('text');
-		//$this->getElement('status');
+		$st_j->addField('to');
+		$st_j->addField('to_id');
+		$st_j->addField('to_email');
+
+		$st_j->addField('cc')->type('text');
+		// $st_j->addField('bcc')->type('text');
+
+		$st_j->addField('subject');
+		$st_j->addField('message')->type('text');
+
+		$st_j->addField('priority')->enum(array('Low','Medium','High','Urgent'))->defaultValue('Medium')->mandatory(true);
+
+		$st_j->hasMany('xepan\crm\Ticket_Comments','ticket_id',null,'Comments');
+		$st_j->hasMany('xepan\crm\Ticket_Attachment','ticket_id',null,'TicketAttachments');
+
 		$this->getElement('status')->defaultValue('Pending');
 		$this->getElement('created_at')->defaultValue($this->app->now);
-		$this->addCondition('type','SupportTicket');
 
 
 	}
