7594a38659a28e3b49123f44a305c25ecedd5dc4
vijay mali <guddavijay@gmail.com>
2016-04-08 17:15:18 +0530

    add menu config

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -14,7 +14,8 @@ class Initiator extends \Controller_Addon {
 
         if($this->app->is_admin){
 	        $m = $this->app->top_menu->addMenu('Crm');
-	        $m->addItem('SupportTicket','xepan_crm_supportticket');
+            $m->addItem('SupportTicket','xepan_crm_supportticket');
+	        $m->addItem('Configuration','xepan_crm_config');
             $cont = $this->add('xepan\crm\Controller_FilterEmails');
 
             $this->app->addHook('emails_fetched',[$cont,'emails_fetched']);
