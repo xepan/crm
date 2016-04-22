6bd76e3eaacece416abe8fd56b5e159c23f6c67e
Nillam Joshi <nilam.joshi49@gmail.com>
2016-04-22 15:20:05 +0530

    add icon..

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -15,7 +15,7 @@ class Initiator extends \Controller_Addon {
         if($this->app->is_admin){
 	        $m = $this->app->top_menu->addMenu('Crm');
             $m->addItem(['SupportTicket','icon'=>'fa fa-file-text-o'],'xepan_crm_supportticket');
-	        $m->addItem(['Configuration','icon'=>'fa fa-dashboard'],'xepan_crm_config');
+	        $m->addItem(['Configuration','icon'=>'fa fa-cog fa-spin'],'xepan_crm_config');
             $cont = $this->add('xepan\crm\Controller_FilterEmails');
 
             $this->app->addHook('emails_fetched',[$cont,'emails_fetched']);
