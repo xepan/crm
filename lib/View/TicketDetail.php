3df7ea3087827643c1906de244734b5addd1cf36
vijay mali <guddavijay@gmail.com>
2016-04-27 11:51:42 +0530

    create view TicketDetail & view template

---
+++ lib/View/TicketDetail.php
@@ -0,0 +1,31 @@
+<?php
+namespace xepan\crm;
+class View_TicketDetail extends \View{
+	function init(){
+		parent::init();
+	}
+	function setModel($model){
+		$m=parent::setModel($model);
+		$to_mail=json_decode($m['to_email'],true);
+		
+		$to_lister=$this->add('CompleteLister',null,'to_lister',['view/grid/ticketdetail-grid','to_lister']);
+		// var_dump($to_mail);
+		// exit;
+		$to_lister->setSource($to_mail);
+		
+		if($model['cc']){
+			$cc_raw=json_decode($m['cc'],true);
+			$cc_lister=$this->add('CompleteLister',null,'cc_lister',['view/grid/ticketdetail-grid','cc_lister']);
+			$cc_lister->setSource($cc_raw);
+		}
+
+		$this->template->setHTML('collapse_header',strip_tags($model['message']));
+		$this->template->setHTML('email_body',$model['message']);
+		
+		return $m;
+
+	}
+	function defaultTemplate(){
+		return ['view/grid/ticketdetail-grid'];
+	}
+}
