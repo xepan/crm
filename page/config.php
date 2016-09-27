da49df08d75114f6b27c21b727d85940ae128a06
vijay mali <guddavijay@gmail.com>
2016-09-27 17:44:13 +0530

    crm email config set value & get value from Json Model

--- page/config.php
+++ page/config.php
@@ -6,52 +6,74 @@ class page_config extends \xepan\base\Page{
 	function init(){
 		parent::init();
 		/*Auto Reply Email Content*/
-		$auto_config = $this->app->epan->config;
-		$auto_subject = $auto_config->getConfig('TICKET_GENERATED_EMAIL_SUBJECT');
-		$auto_body = $auto_config->getConfig('TICKET_GENERATED_EMAIL_BODY');
+
+		$config_m = $this->add('xepan\base\Model_ConfigJsonModel',
+		[
+			'fields'=>[
+						'auto_reply_subject'=>'Line',
+						'auto_reply_body'=>'xepan\base\RichText',
+						'denied_email_subject'=>'Line',
+						'denied_email_body'=>'xepan\base\RichText',
+						'closed_email_subject'=>'Line',
+						'closed_email_body'=>'xepan\base\RichText',
+						],
+				'config_key'=>'SUPPORT_SYSTEM_CONFIG',
+				'application'=>'crm'
+		]);
+		$config_m->add('xepan\hr\Controller_ACL');
+		$config_m->tryLoadAny();
+
+		// $auto_config = $this->app->epan->config;
+		// $auto_subject = $auto_config->getConfig('TICKET_GENERATED_EMAIL_SUBJECT');
+		// $auto_body = $auto_config->getConfig('TICKET_GENERATED_EMAIL_BODY');
 		$form=$this->add('Form',null,'auto-reply');
-		$form->addField('line','subject')->set($auto_subject)->setFieldHint('{$token}, {$title}');
-		$form->addField('xepan\base\RichText','body')->set($auto_body)->setFieldHint('{$contact_name}, {$token}, {$sender_email_id}');
+		$form->setModel($config_m,['auto_reply_subject','auto_reply_body']);
+		$form->getElement('auto_reply_subject')->set($config_m['auto_reply_subject'])->setFieldHint('{$token}, {$title}')->setCaption('Subject');
+		$form->getElement('auto_reply_body')->set($config_m['auto_reply_body'])->setFieldHint('{$contact_name}, {$token}, {$sender_email_id}')->setCaption('Body');
 		$form->addSubmit('Update')->addClass('btn btn-primary');
 
 		if($form->isSubmitted()){
-			$auto_config->setConfig('TICKET_GENERATED_EMAIL_SUBJECT',$form['subject'],'crm');
+			$form->save();
+			// $auto_config->setConfig('TICKET_GENERATED_EMAIL_SUBJECT',$form['subject'],'crm');
 
-			$auto_config->setConfig('TICKET_GENERATED_EMAIL_BODY',$form['body'],'crm');
+			// $auto_config->setConfig('TICKET_GENERATED_EMAIL_BODY',$form['body'],'crm');
 			$form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
 		}
 
 		/*Reject Mail Content*/
 
-		$reject_config = $this->app->epan->config;
-		$reject_subject = $reject_config->getConfig('SUPPORT_EMAIL_DENIED_SUBJECT');
-		$reject_body = $reject_config->getConfig('SUPPORT_EMAIL_DENIED_BODY');
+		// $reject_config = $this->app->epan->config;
+		// $reject_subject = $reject_config->getConfig('SUPPORT_EMAIL_DENIED_SUBJECT');
+		// $reject_body = $reject_config->getConfig('SUPPORT_EMAIL_DENIED_BODY');
 		$form=$this->add('Form',null,'reject-reply');
-		$form->addField('line','subject')->set($reject_subject);
-		$form->addField('xepan\base\RichText','body')->set($reject_body)->setFieldHint('{$sender_email_id}');
+		$form->setModel($config_m,['denied_email_subject','denied_email_body']);
+		$form->getElement('denied_email_subject')->set($config_m['denied_email_subject'])->setCaption('Subject');
+		$form->getElement('denied_email_body')->set($config_m['denied_email_body'])->setFieldHint('{$sender_email_id}')->setCaption('Body');
 		$form->addSubmit('Update')->addClass('btn btn-primary');
 
 		if($form->isSubmitted()){
-			$reject_config->setConfig('SUPPORT_EMAIL_DENIED_SUBJECT',$form['subject'],'crm');
+			$form->save();
+			// $reject_config->setConfig('SUPPORT_EMAIL_DENIED_SUBJECT',$form['subject'],'crm');
 
-			$reject_config->setConfig('SUPPORT_EMAIL_DENIED_BODY',$form['body'],'crm');
+			// $reject_config->setConfig('SUPPORT_EMAIL_DENIED_BODY',$form['body'],'crm');
 			$form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
 		}
 		/*Closed / Complete  Ticket Mail Content*/
 
-		$close_config = $this->app->epan->config;
-		$close_subject = $close_config->getConfig('SUPPORT_EMAIL_CLOSED_TICKET_SUBJECT');
-		$close_body = $close_config->getConfig('SUPPORT_EMAIL_CLOSED_TICKET_BODY');
+		// $close_config = $this->app->epan->config;
+		// $close_subject = $close_config->getConfig('SUPPORT_EMAIL_CLOSED_TICKET_SUBJECT');
+		// $close_body = $close_config->getConfig('SUPPORT_EMAIL_CLOSED_TICKET_BODY');
 		$form=$this->add('Form',null,'close-reply');
-		$form->addField('line','subject')->set($close_subject)->setFieldHint('{$token}, {$title}');
-		$form->addField('xepan\base\RichText','body')->set($close_body)->setFieldHint('{$sender_email_id} {$token} {$title}');
+		$form->setModel($config_m,['closed_email_subject','closed_email_body']);
+		$form->getElement('closed_email_subject')->set($config_m['closed_email_subject'])->setFieldHint('{$token}, {$title}')->setCaption('Subject');
+		$form->getElement('closed_email_body')->set($config_m['closed_email_body'])->setFieldHint('{$sender_email_id} {$token} {$title}')->setCaption('Body');
 		$form->addSubmit('Update')->addClass('btn btn-primary');
 
 		if($form->isSubmitted()){
-			$close_config->setConfig('SUPPORT_EMAIL_CLOSED_TICKET_SUBJECT',$form['subject'],'crm');
+			// $close_config->setConfig('SUPPORT_EMAIL_CLOSED_TICKET_SUBJECT',$form['subject'],'crm');
 
-			$close_config->setConfig('SUPPORT_EMAIL_CLOSED_TICKET_BODY',$form['body'],'crm');
-			
+			// $close_config->setConfig('SUPPORT_EMAIL_CLOSED_TICKET_BODY',$form['body'],'crm');
+			$form->save();
 			$form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
 		}
 	}
